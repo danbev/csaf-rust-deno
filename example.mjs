f0d57bc139c5d8f5dc784fccc0356ba34da82302
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-02-05 07:52:05 +0100

    src: remove commented out imports
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- example.mjs
+++ example.mjs
@@ -1,10 +1,6 @@
 import validateStrict from '@secvisogram/csaf-validator-lib/validateStrict.js';
 import * as mandatory from '@secvisogram/csaf-validator-lib/mandatoryTests.js';
 import { optionalTest_6_2_1 } from '@secvisogram/csaf-validator-lib/optionalTests.js';
-
-//import validateStrict from '../csaf-validator-lib/validateStrict.js'
-//import * as mandatory from '../csaf-validator-lib/mandatoryTests.js'
-//import { optionalTest_6_2_1 } from '../csaf-validator-lib/optionalTests.js'
 import { csaf_2_0_strict } from '@secvisogram/csaf-validator-lib/schemaTests.js'
 
 const document = {}
